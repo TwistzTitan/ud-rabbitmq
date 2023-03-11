0715faf75622de6e089707624da9fde8b4baf61f
Leonardo Honorio <root@DESKTOP-8A8H1TN.localdomain>
2023-03-11 12:30:53 -0300

    feat: basic sender and receiver

---
+++ Producer/Sender.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer 
+{ 
+
+    public class Sender
+    {   
+        private static string host = "localhost";
+        
+        public static void Main(string [] args)
+        {
+            var factory = new ConnectionFactory(){ HostName = host };
+            using(var conn = factory.CreateConnection())
+            using(var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare("BasicRabbitTest",false,false,false,null);
+
+                string message = "Hello world with RabbitMQ .Net Core!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("","BasicTest",null,body);
+
+                Console.WriteLine("Publish succeed.\n Message: {0}", message);
+            }
+            Console.WriteLine("Press [enter] to exit Sender");
+            Console.ReadLine();
+        }
+    }
+
+}
